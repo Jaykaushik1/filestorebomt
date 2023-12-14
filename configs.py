dde9f1ec704daec5bbaebbd22e0f53dc954d3d38
Jaykaushik1 <153753677+Jaykaushik1@users.noreply.github.com>
2023-12-14 16:38:51 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,13 +4,13 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
+	API_ID = int(os.environ.get("API_ID", 11906002))
+	API_HASH = os.environ.get("API_HASH", "9e2473a5d38c67d0fa918e04a8889a8c")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6313837168:AAFvi6MX2QDsXCWe9Bi2w_R_xsxN3dYCdGI")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "@BOBDEATH73BOT")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ottbot:ottbot@cluster0.acu0ll3.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
